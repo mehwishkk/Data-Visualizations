eaa3c8becc1edd65657c3ab0d058c67947d8c919
MEHWISH <86172399+mehwishkk@users.noreply.github.com>
2025-05-05 22:27:43 +0530

    Update language_popularity_plot.py

--- language_popularity_plot.py
+++ language_popularity_plot.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 x=["python", "c", "c++" ,"Java"]
 y=[85,70,60,82]
 plt.xlabel("language", fontsize=15)
-plt.ylabel("No.")
+plt.ylabel("No. of users")
 plt.title("Programming Language Popularity")
 plt.bar(x,y)
 plt.show()
